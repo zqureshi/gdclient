1f0753b6206412accf0ca77d97a0ceeba54a3bcf
Zeeshan Qureshi <zee@zqureshi.in>
2018-05-30 02:04:51 -0400

    Implement DNS lookup

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"fmt"
+	"github.com/miekg/dns"
 )
 
 type httpbinResponse struct {
@@ -13,28 +14,65 @@ type httpbinResponse struct {
 }
 
 func main() {
-	// 1. Fetch current IP from httpbin.org
-	resp, err := http.Get("https://httpbin.org/ip")
+	hostIP, err := getHostIP()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	domainIP, err := getDomainIP("home.zeeshanq.com.")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("IP of current host is " + hostIP)
+	fmt.Println("IP of domain is " + domainIP)
+
+	if domainIP != hostIP {
+		fmt.Println("Updating domain IP to " + hostIP)
+		// TODO implement and finish off
+	} else {
+		fmt.Println("IPs match, nothing to update")
+	}
+}
+
+func getHostIP() (string, error) {
+	resp, err := http.Get("https://httpbin.org/ip")
+	if err != nil {
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Fatal(resp)
+		return "", err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	hResp := httpbinResponse{}
 	err = json.Unmarshal(body, &hResp)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	currentIp := hResp.Origin
-	fmt.Println(currentIp)
+	return hResp.Origin, nil
+}
+
+func getDomainIP(domain string) (string, error) {
+	m := new(dns.Msg)
+	m.SetQuestion(domain, dns.TypeA)
+
+	in, err := dns.Exchange(m, "1.1.1.1:53")
+	if err != nil {
+		return "", err
+	}
+
+	a, ok := in.Answer[0].(*dns.A);
+	if !ok {
+		return "", fmt.Errorf("could not read DNS answer: %s", in)
+	}
+
+	return a.A.String(), nil
 }
